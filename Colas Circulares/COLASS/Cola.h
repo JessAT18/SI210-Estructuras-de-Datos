8a67646bf7b2ab07b9af056e7e8210e9c92db392
Jessica Aquino <j-aquino@outlook.com>
2019-04-25 10:18:23 -0400

    Colas circulares

---
+++ Colas Circulares/COLASS/Cola.h
@@ -0,0 +1,85 @@
+#pragma once
+#include "Persona.h"
+#include <iostream>
+using namespace std;
+const int n=20;
+class Cola
+{
+private:
+    Persona cola[n];
+    int frente;
+    int final;
+
+public:
+    Cola(void)
+    {
+        frente=0;
+        final=0;
+    }
+
+    int Get_frente()
+    {
+        return frente;
+    }
+    void Set_frente (int f)
+    {
+        frente=f;
+    }
+
+    int Get_final()
+    {
+        return final;
+    }
+    void Set_final(int f)
+    {
+        final=f;
+    }
+
+    bool Cola_vacia()
+    {
+        if (frente==final)
+            return true;
+        else
+            return false;
+    }
+
+    bool Cola_llena()
+    {
+        if (frente==(final+1)%n)
+            return true;
+        else
+            return false;
+    }
+
+    bool Insertar(Persona x)
+    {
+        if (Cola_llena()==true)
+            return false;
+        final=(final+1)%n;
+        cola[final]=x;
+        return true;
+    }
+
+    bool Eliminar (Persona &x)
+    {
+        if(Cola_vacia()==true)
+            return false;
+        else
+        {
+            frente=(frente+1)%n;
+            x=cola[frente];
+            return true;
+        }
+    }
+
+    Cola Apuntador()
+    {
+        return *this;
+    }
+
+    void This_cola (Cola x)
+    {
+        *this=x;
+    }
+};
+
