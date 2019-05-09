e3889ee2bc39ba1a4573f88ae7d7d963fb2ddd89
Jessica Aquino <j-aquino@outlook.com>
2019-05-09 10:14:25 -0400

    Eliminando la version anterior de listas encadenadas

--- 5 3 2019 Listas encadenadas/5 3 2019 Listas encadenadas/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp: archivo de código fuente que contiene sólo las inclusiones estándar
-// 5 3 2019 Listas encadenadas.pch será el encabezado precompilado
-// stdafx.obj contiene la información de tipos precompilada
-
-#include "stdafx.h"
-
-
